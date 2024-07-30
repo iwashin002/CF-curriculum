2a1889b8d819cf880ee5a7e584e9a648c03d19a3
iwashin002 <0804pgageha02@gmail.com>
2024-07-30 11:08:28 +0900

    7月30日JS  L6

--- JavaScript/JS-lesson/js-lesson6.js
+++ JavaScript/JS-lesson/js-lesson6.js
@@ -8,3 +8,9 @@ console.log('私は、'+Lastname);
 
 //上記のコードの書き方をまねして、実際に変数を宣言 → 出力 → 再代入 → 出力してみましょう！
 //この下にコードを書いてね！
+
+let Hobby = '読書';
+console.log('私の趣味は'+Hobby);
+
+Hobby = '映画鑑賞';
+console.log('趣味は'+Hobby)
