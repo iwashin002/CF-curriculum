e00466dcd2b148af297f680eda058e3aa87bbf8b
iwashin002 <0804pgageha02@gmail.com>
2025-04-08 09:46:52 +0900

    4月8日ログイン機能

--- lesson6-laravel/laravel-project/app/Models/Contact.php
+++ lesson6-laravel/laravel-project/app/Models/Contact.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone_number',
+        'email'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
