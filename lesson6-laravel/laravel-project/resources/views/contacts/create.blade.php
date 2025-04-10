05c71bd4e546294d7f9866c1526e89840f0d786a
iwashin002 <0804pgageha02@gmail.com>
2025-04-10 22:58:51 +0900

    4月10日連絡先登録機能5

--- lesson6-laravel/laravel-project/resources/views/contacts/create.blade.php
+++ lesson6-laravel/laravel-project/resources/views/contacts/create.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
     <h1>Create New Contact</h1>
-    <form>
+    <form action="{{ route('contact.store') }}" method="post">
         @csrf
         <label for="name">Name:</label>
-        <!-- この行にinputを追加 -->
+        <input type="text" name="name" id="name">
+
         <label for="phone_number">Phone Number:</label>
-        <!-- この行にinputを追加 -->
+        <input type="text" name="phone_number" id="phone_number">
+
         <label for="email">Email:</label>
-        <!-- この行にinputを追加 -->
+        <input type="email" name="email" id="email">
+        
         <button type="submit">Create</button>
     </form>
 @endsection
